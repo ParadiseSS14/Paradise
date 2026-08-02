9083442a38582f42adef78533757b52f9f08e6ba
Pythbit <exgrahamp@gmail.com>
2026-08-02 08:17:49 -0700

    blindedbyflashingsystem and acid immunity

---
+++ Content.Shared/Flash/Components/_Paradise/BlindedByFlashingComponent.cs
@@ -0,0 +1,19 @@
+using Content.Shared.Eye.Blinding.Systems;
+using Robust.Shared.GameStates;
+
+namespace Content.Shared.Flash.Components;
+
+/// <summary>
+/// This entity will take eye damage from flashes.
+/// Copied from BlindedByFlashingComponent
+/// </summary>
+[RegisterComponent, NetworkedComponent]
+[Access(typeof(BlindedByFlashingSystem))]
+public sealed partial class BlindedByFlashingComponent : Component
+{
+    /// <summary>
+    /// How much damage it will take.
+    /// </summary>
+    [DataField(required: true)]
+    public int EyeDamage;
+}

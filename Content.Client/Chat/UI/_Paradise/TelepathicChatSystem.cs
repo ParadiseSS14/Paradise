fb9046cd26a774a0e04e1f29b2c8cc8af9dee6b9
Pythbit <exgrahamp@gmail.com>
2026-08-08 08:26:57 -0700

    wrong client system namespace

--- Content.Client/Chat/UI/_Paradise/TelepathicChatSystem.cs
+++ Content.Client/Chat/UI/_Paradise/TelepathicChatSystem.cs
@@ -1,7 +1,7 @@
 using Content.Client.Chat.UI._Paradise;
 using Content.Shared.Chat._Paradise;
 
-namespace Content.Server.Client._Paradise;
+namespace Content.Client.Client._Paradise;
 
 public sealed partial class TelepathicChatSystem : EntitySystem
 {

5b56b862600d6e35f463f39a3c3d8d0d34c7df4e
The Malf E <spessengineer1@gmail.com>
2026-08-09 15:13:40 -0500

    does pollard's commands

--- Content.Shared/Disposal/Router/DisposalRouterSystem.cs
+++ Content.Shared/Disposal/Router/DisposalRouterSystem.cs
@@ -75,8 +75,7 @@ public sealed partial class DisposalRouterSystem : EntitySystem
             _audio.PlayPredicted(ent.Comp.ClickSound, ent, msg.Actor, AudioParams.Default.WithVolume(-2f));
         }
     }
-//Paradise Content - Mail Sorters
-
+// Paradise Content - Mail Sorters
     public void SetTags(Entity<DisposalRouterComponent> router, HashSet<string> tags)
     {
         router.Comp.Tags.Clear();

08a3586cc4546117b615c1e67b969e3b87b9dc44
The Malf E <spessengineer1@gmail.com>
2026-08-19 01:18:23 -0500

    first sofa changes and anchorable system changes

---
+++ Content.Client/_Paradise/ChairArmrest/ChairArmrestSystem.cs
@@ -0,0 +1,52 @@
+﻿using System.Linq;
+using Content.Shared._Paradise.ChairArmrest;
+using Content.Shared.Buckle.Components;
+using Robust.Client.GameObjects;
+using Robust.Client.Graphics;
+using Robust.Shared.Timing;
+
+namespace Content.Client._Paradise.ChairArmrest;
+
+public sealed partial class ChairArmrestSystem : EntitySystem
+{
+    [Dependency] private SpriteSystem _spriteSystem = default!;
+    [Dependency] private IGameTiming _gameTiming = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<ChairArmrestComponent, ComponentStartup>(OnInit);
+        SubscribeLocalEvent<ChairArmrestComponent, StrappedEvent>(OnStrap);
+        SubscribeLocalEvent<ChairArmrestComponent, UnstrappedEvent>(OnUnstrap);
+    }
+
+    private void OnInit(EntityUid uid, ChairArmrestComponent component, ComponentStartup args)
+    {
+        var xform = Transform(uid);
+        var coordinates = xform.Coordinates;
+        var overlay = Spawn("SofaArmrestOverlay", coordinates);
+
+        if(TryComp<SpriteComponent>(overlay, out SpriteComponent? sprite))
+        {
+            _spriteSystem.LayerSetRsiState((overlay, sprite), 0, new RSI.StateId(component.ArmrestOverlay));
+            component.OverlayEntity = overlay;
+        }
+    }
+
+    private void OnStrap(EntityUid uid, ChairArmrestComponent component, StrappedEvent args)
+    {
+        if(TryComp<SpriteComponent>(component.OverlayEntity, out SpriteComponent? sprite) && _gameTiming.IsFirstTimePredicted)
+        {
+            _spriteSystem.SetVisible((component.OverlayEntity.Value, sprite), true);
+        }
+    }
+
+    private void OnUnstrap(EntityUid uid, ChairArmrestComponent component, UnstrappedEvent args)
+    {
+        if(TryComp<SpriteComponent>(component.OverlayEntity, out SpriteComponent? sprite) && _gameTiming.IsFirstTimePredicted)
+        {
+            _spriteSystem.SetVisible((component.OverlayEntity.Value, sprite), false);
+        }
+    }
+}
---
+++ Content.Shared/_Paradise/ChairArmrest/ChairArmrestComponent.cs
@@ -0,0 +1,10 @@
+﻿namespace Content.Shared._Paradise.ChairArmrest;
+
+[RegisterComponent]
+public sealed partial class ChairArmrestComponent : Component
+{
+    [DataField]
+    public required string ArmrestOverlay;
+
+    public EntityUid? OverlayEntity;
+}
